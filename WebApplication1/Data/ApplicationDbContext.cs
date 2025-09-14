c2655eff417a438190800cde48bf192ffc214e54
KingB <510king510@gmail.com>
2025-09-13 17:37:17 -0700

    Added Teacher menu6

--- WebApplication1/Data/ApplicationDbContext.cs
+++ WebApplication1/Data/ApplicationDbContext.cs
@@ -10,6 +10,8 @@ namespace WebApplication1.Data
             : base(options)
         {
         }
-        public DbSet<WebApplication1.Models.Student> Student { get; set; } = default!;
+
+        public DbSet<Teacher> Teacher { get; set; }
+        public DbSet<Student> Student { get; set; }
     }
 }
---
+++ WebApplication1/Models/Teacher.cs
@@ -0,0 +1,13 @@
+﻿namespace WebApplication1.Models
+
+{
+    public class Teacher
+    {
+        public int Id { get; set; }
+        public required string FirstName { get; set; } = string.Empty;
+        public required string LastName { get; set; } = string.Empty;
+        public required string Qualifications { get; set; }
+        public required string Email { get; set; }
+        public string? ClassesTaught { get; set; }
+    }
+}
