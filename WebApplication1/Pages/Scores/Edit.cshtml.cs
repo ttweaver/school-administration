02fd15ccdb1cf4dccd6e9d64f4e2617397c5b1cb
Cess <cescelie.spirkobeene@student.ctuonline.edu>
2025-09-19 03:43:01 -0400

    Added drop down menus to Score Type and Title

--- WebApplication1/Pages/Scores/Edit.cshtml.cs
+++ WebApplication1/Pages/Scores/Edit.cshtml.cs
@@ -6,7 +6,6 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
-using WebApplication1.Data;
 using WebApplication1.Models;
 
 namespace WebApplication1.Pages.Scores
